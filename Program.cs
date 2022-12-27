5bcdd2c42ea7b148d4a19a9517c0762dc458ae0d
arulifet <arulifet@gmail.com>
2022-12-27 21:10:51 +0530

    added k variable

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿class Program{
   static void Main(string[] args){
     int j = 1; // j has been declared.
-    int i = j*1 ;
+    int i = j+2 ;
+    int k = 1;
 
 
     Console.WriteLine("Hello World, Welcome to .NET Framework");
