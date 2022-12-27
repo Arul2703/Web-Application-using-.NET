b9f7dc5e216a78db895390e2f46bfbe622452649
arulifet <arulifet@gmail.com>
2022-12-27 21:06:51 +0530

    switched to multiply

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿class Program{
   static void Main(string[] args){
     int j = 1; // j has been declared.
-    int i = j+1 ;
+    int i = j*1 ;
 
 
     Console.WriteLine("Hello World, Welcome to .NET Framework");
