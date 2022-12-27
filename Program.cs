0769563341f1527fd049a1f76fa1c63758e273bc
arulifet <arulifet@gmail.com>
2022-12-27 20:54:11 +0530

    some files had been added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿class Program{
   static void Main(string[] args){
-    int i = 10;
+    int i = 20;
     Console.WriteLine("Hello World, Welcome to .NET Framework");
+    Console.WriteLine("Let's try to create a web application using .NET.");
   }
 }
